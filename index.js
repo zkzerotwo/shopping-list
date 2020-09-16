bfb7a7d6bbf50a43bf82db98b2e694bb08d60d56
Christopher 'ZK' Hal <halc713@gmail.com>
2020-09-16 15:02:48 -0500

    Addding items
    item functionality adding and deleting complete

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ $(function() {
     $('.shopping-list').append(listSelection);
 
   });
+  $('.shopping-list').on("click", '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item__checked')
+  });
 });
 
 
